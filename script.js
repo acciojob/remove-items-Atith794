439effcc65b7987a6953e31474b649bdb9de78bf
Atith794 <68183452+Atith794@users.noreply.github.com>
2023-10-30 21:45:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your JS code here. If required.
+// let btn = document.getElementsByTagName("input");
+// btn.addEventListener("click",()=>{
+// 	let ele = document.getElementById("colorSelect");
+// 	// let ili = ele.querySelector("option");
+// 	// ili.deleteElementsByTagName("option");
+// 	// ele.deleteElementsByTagName("option");
+// 	ele.getElementsByTagName("option").remove();
+// })
+
+let colorSelect = document.getElementById("colorSelect");
+let removeBtn = document.querySelector('input[type="button"]');
+
+removeBtn.addEventListener("click",function(){
+	
+	let selectedOptn = colorSelect.options[colorSelect.selectedIndex];
+
+	if(selectedOptn){
+		colorSelect.removeChild(selectedOptn);
+	}
+});
